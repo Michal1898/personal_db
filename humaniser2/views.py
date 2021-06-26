c3495e145cd796ad90eec2dd35d32176f8ced4de
Michal1898 <michal.volf@gmail.com>
2021-06-26 23:40:31 +0200

    new humaniser API added.

--- humaniser2/views.py
+++ humaniser2/views.py
@@ -69,35 +69,39 @@ class PeopleView(View):
 
 
 class PeopleListView(ListView):
-  # Person.objects.all().delete()
-  # n=requests.get('https://humaniser2.herokuapp.com/?sex=female&count=10')
-  # n=n.json()
+  Person.objects.all().delete()
+  n=requests.get('https://humaniser3.herokuapp.com/?sex=male&count=20&min=5&max=7')
+  n=n.json()
 
-  # for x in range(len(n)):
-  #   m = n[x]
-  #   print(m)
-  #   firstname = m.get("firstname")
-  #   surname = m.get("surname")
-  #   gender = m.get("gender")
-  #   birthdate = m.get("birthdate")
-  #   city = m.get("city")
-  #   street = m.get("street")
-  #   if street == "":
-  #     street = city
-  #   house_no = m.get("house_no")
-  #   plz = m.get("plz")
-  #   print(firstname, surname, gender, birthdate, city, street, house_no, plz)
-  #   person_obj = Person(
-  #     firstname=firstname,
-  #     surname=surname,
-  #     gender=gender,
-  #     birthdate=datetime.date.today(),
-  #     city=city,
-  #     street=street,
-  #     house_no=house_no,
-  #     plz=plz)
-  #
-  #   person_obj.save()
+  for x in range(len(n)):
+    m = n[x]
+    print(m)
+    firstname = m.get("firstname")
+    surname = m.get("surname")
+    gender = m.get("gender")
+    birth_day = m.get("day_of_birth")
+    birth_month = m.get("month_of_birth")
+    birth_year = m.get("year_of_birth")
+    city = m.get("city")
+    street = m.get("street")
+    if street == "":
+      street = city
+    house_no = m.get("house_no")
+    plz = m.get("plz")
+    print(firstname, surname, gender, birth_day, birth_month, birth_year, city, street, house_no, plz)
+    person_obj = Person(
+      firstname=firstname,
+      surname=surname,
+      gender=gender,
+      birth_day=birth_day,
+      birth_month=birth_month,
+      birth_year=birth_year,
+      city=city,
+      street=street,
+      house_no=house_no,
+      plz=plz)
+
+    person_obj.save()
 
 
 
